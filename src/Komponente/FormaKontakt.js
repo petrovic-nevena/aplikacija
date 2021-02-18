b56a8290bd8d72f05a824dba497e0cad47799119
petrovic-nevena <np20170037@student.fon.bg.ac.rs>
2021-02-18 17:34:04 +0100

    2.Commit-Kontakt forma

---
+++ src/Komponente/FormaKontakt.js
@@ -0,0 +1,50 @@
+import React from 'react';
+/* import axios from 'axios'; */
+
+class FormaKontakt extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {            
+            email: '',
+            comment: '',
+        }
+    }
+
+
+    resetForm() {
+        this.setState({ email: '', coment: '' })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form id="contact-form" >                    
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Email:</label>
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Komentar:</label>
+                        <textarea className="form-control" rows="5" id="message" value={this.state.coment} onChange={this.onMessageChange.bind(this)} />
+                    </div>
+                    <div className="form-group">
+                    <button type="submit" className="btn btn-primary" onClick={() => alert('Vaš komentar je prosleđen. Email: ' + this.state.email + ', komentar: '+ this.state.comment )}> Prosledi komentar </button>
+                    </div>
+                </form>
+            
+            </div>
+        );
+    }
+    
+
+    onEmailChange(event) {
+        this.setState({ email: event.target.value })
+    }
+
+    onMessageChange(event) {
+        this.setState({ comment: event.target.value })
+    }
+}
+
+export default FormaKontakt;
--- src/stranice/contact.js
+++ src/stranice/contact.js
@@ -1,19 +1,19 @@
 import React from 'react'
+import FormaKontakt from '../Komponente/FormaKontakt';
 
-const Contact = () => {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '90vh'
-        }}
-      >
-        <h1>Contact Us</h1>
-      </div>
-    );
-  };
+function Contact(){
+    const kontakt={
+        email: "yamifood@gmail.com"
+    }
+    return(
+        <div className="container" style={{marginTop: 2 + 'em'}}>
+            <h3>U sledećoj formi možete ostaviti Vaš komentar</h3>            
+            <FormaKontakt/>
+            <p>Za bilo kakve dodatne informacije možete nam pisati na mejl  {kontakt.email} </p>
+            <br></br>
+        </div>
+    )
+}
   
   export default Contact;
   
